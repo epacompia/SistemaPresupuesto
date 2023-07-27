b5b86a9179cd560a3387965d5f4e4d5d3ce91517
eduardopacompialopez <eduardopacompialopez@gmail.com>
2023-07-27 16:36:40 -0500

    completando validaciones basicas

---
+++ SistemaPresupuesto/Controllers/TiposCuentasController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SistemaPresupuesto.Models;
+
+namespace SistemaPresupuesto.Controllers
+{
+    public class TiposCuentasController : Controller
+    {
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Crear(TipoCuenta tipoCuenta)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(tipoCuenta);
+            }
+
+            return View() ;
+        }
+    }
+}
